f7d673420f492dc22a65505ee2253e5d90289d32
Caturra <wasdjkl233@gmail.com>
2020-10-29 00:52:15 +0800

    add event support for ctx

--- Context.h
+++ Context.h
@@ -7,34 +7,36 @@
     const static int messageType = __COUNTER__+1 
 
 // 只提供接口部分，该接口会应用于IO复用
-// 虽然设计上是都会post到某个MQ，但作为接口，持有MQ的指针或者send(MQ)都不太必要，因此多提供一个ContextImpl
 class Context {
 public:
+
+// message define
 
     CONTEXT_MSG_DEFINE(MSG_POLL_READ);
     CONTEXT_MSG_DEFINE(MSG_POLL_WRITE);
     CONTEXT_MSG_DEFINE(MSG_POLL_ERROR);
     CONTEXT_MSG_DEFINE(MSG_POLL_CLOSE);
 
+// for handler
+
     virtual void sendReadMessage() {}
     virtual void sendWriteMessage() {}
     virtual void sendErrorMessage() {}
     virtual void sendCloseMessage() {}
 
-// poll
+// for multiplexer
 
     virtual int fd() const = 0;
     virtual uint32_t events() const = 0;
-    virtual int state() const = 0;
+    virtual void updateState() = 0;
 
-    virtual void updateStatus() {}
+// self permission
+
     virtual void enableRead() {}
     virtual void enableWrite() {}
     virtual void disableRead() {}
     virtual void disableWrite() {}
-    // update
     // add remove change
-    // status
 };
 
 // using ContextImpl = Context;
--- ContextImpl.h
+++ ContextImpl.h
@@ -6,16 +6,18 @@
 #include "Message.h"
 #include "utils/Pointer.h"
 #include "Looper.h"
+#include "Multiplexer.h"
 
 class Handler;
 
-// 用于简单实现的包装类，如果是相对复杂的实现，应该直接继承Context，并且明确Handler是哪种类型
+// 提供基本的poller接口支持以及handler机制默认实现
+// 具体的Context实现类可以直接继承，并给出fd
 class ContextImpl: public Context {
 public:
     void sendMessage(int what) { _messageQueue->post({_handler.get(), what}); } 
     void sendMessage(int what, int flag) { _messageQueue->post({_handler.get(), what, flag}); }
 
-    // 接口需要手动控制data生命周期
+    // 接口需要手动控制data生命周期，一般搭配Defer食用
     void sendMessageWithData(int what, void *data) { 
         _messageQueue->post( Message {
             .target = _handler.get(),
@@ -34,25 +36,55 @@ public:
     void sendCloseMessage() override { sendMessage(MSG_POLL_CLOSE); }
 
 
-    virtual int state() const { return _state; }
+    uint32_t events() const override { return _events; }
+    void updateState() override {
+        if(_state == STATE_NEW || _state == STATE_DELETED) {
+            _state = STATE_ADDED;
+            _multiplexer->update(EPOLL_CTL_ADD, this);
+        } else { // STATE_ADDED
+            if(_events == EVENT_NONE) {
+                _multiplexer->update(EPOLL_CTL_DEL, this);
+                _state = STATE_DELETED;
+            } else {
+                _multiplexer->update(EPOLL_CTL_MOD, this);
+            }
+        }
+    }
 
-    virtual void updateStatus();
-    virtual void enableRead();
-    virtual void enableWrite();
-    virtual void disableRead();
-    virtual void disableWrite();
+    void enableRead() override { _events |= EVENT_READ; }
+    void enableWrite() override { _events |= EVENT_WRITE; }
+    void disableRead() override { _events &= ~EVENT_READ; }
+    void disableWrite() override { _events &= ~EVENT_WRITE; }
 
     ContextImpl(Handler *handler = nullptr, Looper *looper = nullptr)
         : _handler(handler),
-          _messageQueue(looper ? looper->getProvider() : nullptr) { }
+          _messageQueue(looper ? looper->getProvider() : nullptr),
+          _multiplexer(looper ? looper->getPoller() : nullptr),
+          _state(STATE_NEW), _events(EVENT_NONE) { }
 
     // fd()
 
 protected:
     Pointer<Handler> _handler;
     Pointer<MessageQueue> _messageQueue;
+    Pointer<Multiplexer> _multiplexer;
 
     int _state;
+    uint32_t _events;
+
+// for state
+
+    constexpr static int STATE_NEW = 0;
+    constexpr static int STATE_ADDED = 1;
+    constexpr static int STATE_DELETED = 2;
+
+// for events
+
+    constexpr static uint32_t EVENT_NONE = 0;
+    constexpr static uint32_t EVENT_READ = 1 << 0;
+    constexpr static uint32_t EVENT_WRITE = 1 << 1;
+    
+    
 };
 
 #endif
