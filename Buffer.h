6d5b39b1cd0073b38a0667ddcfbe2d5628feaf96
Caturra <wasdjkl233@gmail.com>
2020-10-28 20:43:47 +0800

    buffer简单调整

--- Buffer.h
+++ Buffer.h
@@ -15,75 +15,35 @@ private:
 
 public:
 
-    void clear() {
-        _r = _w = 0;
-        // TODO 用_size控制吧
-    }
+// 大小接口
 
-    int size() {
-        return _size;
-    }
+    int size() { return _size; }
+    int rest() { return _r - _w; }  // 还有多少未读取
+    int limit() { return _size - _r; } // 还有多少彻底填满
+    int freeSpace() { return _size - rest(); }
 
-    // 还有多少未读取
-    int rest() {
-        return _r - _w;
-    }
+// 扩容相关
 
-
-    // 还有多少彻底填满
-    int limit() {
-        return _size - _r;
-    }
-
-    int freeSpace() {
-        return _size - rest();
-    }
-
-    void reuse() {
-        _r = _w = 0;
-    }
+    void clear() { _r = _w = 0; }
 
     // 能否复用，尽量延迟resize
     // TODO 当rest()为一个极少占比的时候，尝试copy
-    void reuseIfPossible() {
-        if(_r == _w) {
-            reuse();
-        }
-    }
+    void reuseIfPossible();
 
     // 处理边缘碎片
-    void gc() {
-        if(_w > 0) {
-            // move [_w, _r] to front
-            memmove(_buf.data(), _buf.data() + _w, _r = rest());
-            _w = 0;
-        }
-        
-    }
+    void gc();
+
+    // 尽可能延迟resize
+    // 根据hint来决定gc策略 / 是否gc
+    void gc(int hint);
+
+    // TODO expand不会有额外check，应该放到private
+
+    void expand() { _buf.resize(_size <<= 1); }
+    void expandTo(int size) { _buf.resize(_size = size); }
 
 
-    // lazy resize if it can
-    void gc(int hint) {
-        if(hint <= limit()) return;
-        gc();
-        // still cannot store
-        if(hint > freeSpace()) {
-            // TODO expand    exactly or powerOf2?
-            int appendSize = freeSpace() - hint;
-            int expectSize = _buf.size() + appendSize;
-            expandTo(_size = roundToPowerOfTwo(expectSize));
-        }
-    }
-
-    void expand() {
-        _buf.resize(_size = (_size << 1));
-    }
-
-
-
-    void expandTo(int size) {
-        _buf.resize(_size = size);
-    }
+// IO相关
 
     void read(int n) { 
         // if
@@ -94,17 +54,9 @@ public:
         reuseIfPossible();
     }
 
-    char* getReadBuffer() {
-        return _buf.data() + _r;
-    }
-
-    char* getWriteBuffer() {
-        return _buf.data() + _w;
-    }
-
-    char* end() {
-        return _buf.data() + _buf.size();
-    }
+    char* getReadBuffer() { return _buf.data() + _r; }
+    char* getWriteBuffer() { return _buf.data() + _w; }
+    char* end() { return _buf.data() + _size; }
 
     
 
@@ -116,11 +68,37 @@ public:
 
     // not a pointer
     template <typename T>
-    void append(const T &data) {
-        append(static_cast<const char *>(&data), sizeof(T));
-    }
+    void append(const T &data) { append(static_cast<const char *>(&data), sizeof(T)); }
     
+    // append(T(&)[N])
 
 
 };
+
+
+inline void Buffer::reuseIfPossible() {
+    if(_r == _w) {
+        clear();
+    }
+}
+
+inline void Buffer::gc() {
+    if(_w > 0) {
+        // move [_w, _r] to front
+        memmove(_buf.data(), _buf.data() + _w, _r = rest());
+        _w = 0;
+    }
+}
+
+inline void Buffer::gc(int hint) {
+    if(hint <= limit()) return;
+    gc();
+    // still cannot store
+    if(hint > freeSpace()) {
+        // TODO expand    exactly or powerOf2?
+        int appendSize = freeSpace() - hint;
+        int expectSize = _buf.size() + appendSize;
+        expandTo(/*_size = */roundToPowerOfTwo(expectSize));
+    }
+}
 #endif
