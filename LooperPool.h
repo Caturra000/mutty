f35bf82d94873fde82dac02a1697120cec1aa827
Caturra <wasdjkl233@gmail.com>
2020-10-31 14:57:11 +0800

    add thread support for looperPool

--- LooperPool.h
+++ LooperPool.h
@@ -2,6 +2,7 @@
 #define __LOOPER_POOL_H__
 #include <bits/stdc++.h>
 #include "utils/Algorithms.h"
+#include "utils/ThreadPool.h"
 #include "Looper.h"
 
 // 具体用途是每次accept都分配到固定大小的Looper池中
@@ -10,19 +11,24 @@ class LooperPool {
 public:
     std::unique_ptr<Looper>& pick() {
         auto index = random<size_t>() & N-1;
-        // TODO if nullptr....
-        return _pool[index];
+        return _pool[index]; // ready
     }
 
-    LooperPool() {
+    LooperPool(): _threads(N) {
         constexpr size_t mayBeZero = N & N-1;
         static_assert(mayBeZero == 0, "N must be power of two.");
+        for(auto &looper : _pool) {
+            looper = std::make_unique<Looper>();
+            _threads.execute([&] {looper->loop();});
+        }
     }
+    
+    // TODO 析构问题
 
 
 private:
     std::array<std::unique_ptr<Looper>, N> _pool;
+    ThreadPool _threads;
 };
 
-// using SimpleLooperPool = 
 #endif
