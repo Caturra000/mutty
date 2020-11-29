71ff06412a0ceae5d05b3ecf04606e82fe6fdfc5
Caturra <wasdjkl233@gmail.com>
2020-11-30 01:35:17 +0800

    fix Inet

--- src/net/InetAddress.h
+++ src/net/InetAddress.h
@@ -5,28 +5,31 @@
 #include <netinet/in.h>
 #include <string>
 #include "utils/StringUtils.h"
-// 对地址类的浅层封装，要求大小与sockaddr_in一致 / 内存顶部是sockaddr_in
+// 对地址类的浅层封装，要求大小与sockaddr_in一致
 struct InetAddress {
 public:
-    InetAddress(sockaddr_in address = {}): _address(address) { } // POD不需要move 且成本低不需要const&
-    InetAddress(uint32_t ip, uint16_t port): _address{AF_INET, htons(port), htonl(ip)} { } // TODO 参数需要using隐藏
+    InetAddress(sockaddr_in address = {}): _address(address) { } // POD不需要move
+    InetAddress(uint32_t ip, uint16_t port): _address{AF_INET, ::htons(port), ::htonl(ip)} { }
+    InetAddress(const std::string &ip, uint16_t port): InetAddress(stringToIp(ip), port) {}
     InetAddress(const std::string &address) {
         auto pivot = split(address, ':');
         assert(pivot.size() == 2);
-        InetAddress(stringToIp(address.substr(pivot[0].first, pivot[0].second - pivot[0].first)), 
-                    toDec<uint16_t>(address.substr(pivot[1].first, pivot[1].second - pivot[1].first)));
-    } 
-    std::string toString() { return ipToString() + ":" + std::to_string(_address.sin_port); } // "192.168.0.1:2333"
+        _address = {
+            AF_INET, 
+            ::htons(toDec<uint16_t>(address.substr(pivot[1].first, pivot[1].second - pivot[1].first))),
+            ::htonl(stringToIp(address.substr(pivot[0].first, pivot[0].second - pivot[0].first)))
+        };
+    }
+    std::string toString() { return ipToString() + ":" + std::to_string(::ntohs(_address.sin_port)); } // "192.168.0.1:2333"
     // TODO 用memcpy实现copy/move
 
 private:
 
-    // 实测toString/toIp是正确的，但是在InetAddress中需要考虑网络字节序
 
     // 别问为什么，我故意写的难看懂
     std::string ipToString() {
         std::string s;
-        uint32_t addr = _address.sin_addr.s_addr; 
+        uint32_t addr = ::ntohl(_address.sin_addr.s_addr); 
         for(int chunk = 3; ~chunk; --chunk) {
             s.append(".", 3 > chunk).append(std::to_string((addr >> (chunk << 3)) & 0xff));
         }
@@ -44,7 +47,7 @@ private:
     }
     
     
-    sockaddr_in _address; // 必须在顶部，保证可直接转型
+    sockaddr_in _address; // 保证可直接转型
     
 };
 
