e7a828870bfb7642ada3eafc804ca7673b3ee92a
Caturra <wasdjkl233@gmail.com>
2021-05-23 12:42:19 +0800

    pointer接口修改

--- src/utils/Pointer.h
+++ src/utils/Pointer.h
@@ -3,49 +3,40 @@
 #include <bits/stdc++.h>
 namespace mutty {
 
+// movable
 template <typename T>
 class Pointer {
 public:
-    Pointer(T* ptr = nullptr): _ptr(ptr) { }
-    Pointer(const Pointer &rhs): _ptr(rhs._ptr) { }
-    Pointer(Pointer &&rhs): _ptr(rhs._ptr) { rhs._ptr = nullptr; }
-    Pointer operator++() { return ++_ptr; }
-    Pointer operator++(int) { return _ptr++; }
-    Pointer& operator=(Pointer rhs) {
-        std::swap(_ptr, rhs._ptr);
-        return *this;
-    }
+    constexpr Pointer(T* ptr = nullptr): _ptr(ptr) {}
+    constexpr Pointer(const Pointer &rhs): _ptr(rhs._ptr) {}
+    constexpr Pointer(Pointer &&rhs): _ptr(rhs._ptr) { rhs._ptr = nullptr; }
+    constexpr Pointer operator++() { return ++_ptr; }
+    constexpr Pointer operator++(int) { return _ptr++; }
+    Pointer& operator=(Pointer rhs);
     T& operator*() { return *_ptr; }
     T* operator->() { return _ptr; }
-    explicit operator bool() { return _ptr; }
-    ~Pointer() { }
+    constexpr explicit operator bool() { return _ptr; }
+    ~Pointer() {}
 
-    T* get() { return _ptr; } // 兼容
-
-    template <typename Base>
-    Base* castTo() { 
-        static_assert(std::is_base_of<Base, T>::value, "can only cast to Pointer<Base>/Base*");
-        return _ptr;
-    }
+    template <typename Base> Base* castTo();
+    T* get() { return _ptr; }
 
 protected:
     T* _ptr;
 };
 
+template <typename T>
+inline Pointer<T>& Pointer<T>::operator=(Pointer rhs) {
+    std::swap(_ptr, rhs._ptr);
+    return *this;
+}
 
-
-/*
-    Pointer<int> p = new int[10] {1, 2, 3, 4, 5};
-    std::cout<< *p++ << std::endl;
-    std::cout << *p << std::endl;
-
-    Pointer<std::vector<int>> pv = new std::vector<int>{1,2,3,4,5}; // 如果是std::vector<int>*，最后两个输出是同一地址
-    std::cout << &(*pv) << std::endl;
-    std::cout << (*pv).size() << std::endl;
-    auto pv2 = std::move(pv);
-    std::cout << &(*pv) << std::endl;
-    std::cout << &(*pv2) << std::endl;
-*/
+template <typename T>
+template <typename Base>
+inline Base* Pointer<T>::castTo() { 
+    static_assert(std::is_base_of<Base, T>::value, "can only cast to Pointer<Base>/Base*");
+    return _ptr;
+}
 
 } // mutty
 
